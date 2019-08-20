c46b8135d0173063fe641d26d9b63fa6cc36024f
yooksi <yooksi@zoho.com>
2019-08-20 10:47:34 +0200

    Refactor askStory function

--- trello/Commands.js
+++ trello/Commands.js
@@ -2,20 +2,17 @@
  * Baseline test to see if bot is online.
  * Also a nice easter egg.
  */
-function askStory(posted)
+function askStory(notificaion)
 {
-  var notif = new Notification(posted);
-  if(notif.member().notTrellinator())
+  var notif = new Notification(notificaion);
+  if(Muthur.didUserMention(notif))
   {
-    if (Muthur.isMentioned(notif))
-    {
-      var comment = notif.addedComment();
-      if (new RegExp("what's the story", "i").test(comment.text())) {
-        comment.card().postComment("@" + notif.member().name() + " All systems nominal.");
-      }
-      else if (new RegExp("Status report", "i").test(comment.text())) {
-        comment.card().postComment("@" + notif.member().name() + " Seven bells and all's well.");
-      }
+    var comment = notif.addedComment();
+    if (new RegExp("what's the story", "i").test(comment.text())) {
+      comment.card().postComment("@" + notif.member().name() + " All systems nominal.");
+    }
+    else if (new RegExp("Status report", "i").test(comment.text())) {
+      comment.card().postComment("@" + notif.member().name() + " Seven bells and all's well.");
     }
   }
 }
