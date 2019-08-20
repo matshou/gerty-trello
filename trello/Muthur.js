79c7edd4887a35f18282436421027ccd09a5d775
yooksi <yooksi@zoho.com>
2019-08-20 10:45:51 +0200

    Write some utility methods

--- trello/Muthur.js
+++ trello/Muthur.js
@@ -1,4 +1,4 @@
-function Muthur() {};
+function Muthur() {}
 
 /**
  * Check if Mother was mentioned in the given notification.
@@ -10,10 +10,28 @@ Muthur.isMentioned = function(notif)
     var is_mentioned = false;
     notif.mentionedMembers().each(function(member)
     {
-      if (member.name() == "muthur6000")
+      if (Muthur.is(member))
         is_mentioned = true;
     });
     return is_mentioned;
   }
   else return false;
 };
+
+/**
+ * Returns true if the member resolved from the
+ * given notification mentioned Muthur
+ */
+Muthur.didUserMention = function(notif)
+{
+  return notif.member().notTrellinator() && Muthur.isMentioned(notif);
+}
+
+
+/**
+ * Returns true if the given member is Muthur
+ */
+Muthur.is = function(member)
+{
+  return member.name() == "muthur6000";
+}
